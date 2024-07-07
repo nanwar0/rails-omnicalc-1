167b734cd9049429591d34f3e829f1ba5cc0bdf0
nanwar0 <nina.anwar@chicagobooth.edu>
2024-07-07 19:32:06 +0000

    final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,39 @@ class PagesController < ApplicationController
     end
   end
   
+  def sqrt
+    if params.fetch("status")=="new"
+      render({:template => "pages_templates/sqrt_new"})
+    elsif params.fetch("status")=="results"
+      @number = params.fetch("number").to_f
+      @sqrt = @number ** 0.5
+      render({:template => "pages_templates/sqrt_results"})
+    end
+  end
+
+  def random
+    if params.fetch("status")=="new"
+      render({:template => "pages_templates/random_new"})
+    elsif params.fetch("status")=="results"
+      @minimum = params.fetch("user_min").to_f
+      @maximum = params.fetch("user_max").to_f
+      @result = rand(@minimum..@maximum)
+      render({:template => "pages_templates/random_results"})
+    end
+  end
+
+  def payment
+    if params.fetch("status")=="new"
+      render({:template => "pages_templates/payment_new"})
+    elsif params.fetch("status")=="results"
+      @apr = params.fetch("user_apr").to_f
+      @years = params.fetch("user_years").to_f
+      @r = params.fetch("user_apr").to_f/100/12
+      @n = params.fetch("user_years").to_f*12
+      @pv = params.fetch("user_pv").to_f
+      @p = (@r*@pv)/(1-(1+@r)**(-@n))
+      render({:template => "pages_templates/payment_results"})
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", {:controller => "pages", :action => "home"})
   get("/square/:status", {:controller => "pages", :action => "square"})
+  get("/square_root/:status", {:controller => "pages", :action => "sqrt"})
+  get("/random/:status", {:controller => "pages", :action => "random"})
+  get("/payment/:status", {:controller => "pages", :action => "payment"})
 end
